2383305650466b08d426b0e55b4b9568e1baee2b
scarabator <43741589+scarabator@users.noreply.github.com>
2018-10-24 11:45:15 +0200

    first commit

---
+++ js/DOM.js
@@ -0,0 +1,46 @@
+/*
+      DOM
+      shi-fu-mi
+      mobile-first
+      button effet hover
+      vous / ia et nouvelle partie
+*/
+
+/*
+function choixutilistateur :
+
+-pierre
+-ciseaux
+-feuille
+
+
+*/
+
+
+
+/*
+function random :
+
+-pierre
+-ciseaux
+-feuille
+
+*/
+
+
+/*
+function comparateur :
+
+-choixutilisateur == random > égalité
+
+-choixutilisateur != random > 
+
+si choixutilisateur = feuille et random = pierre > gagné
+si choixutilisateur = feuille et random = ciseaux > perdu
+si choixutilisateur = pierre et random =  ciseaux > gagné
+si choixutilisateur = pierre et random = feuille > perdu
+si choixutilisateur = ciseaux et random = pierre > perdu
+si choixutilisateur = ciseaux et random = feuille > gagné
+
+
+*/
